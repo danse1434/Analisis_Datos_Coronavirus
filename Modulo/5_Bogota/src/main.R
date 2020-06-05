59846a7f5db2947ada316eccb87edef37c3182b3
Daniel Parra <dsparrag@unal.edu.co>
2020-06-04 22:39:26 -0500

    Adición de gráfico de comportamiento en Bogotá

--- Modulo/5_Bogota/src/main.R
+++ Modulo/5_Bogota/src/main.R
@@ -1,6 +1,7 @@
 library("tidyverse")
 library("lubridate")
 library("sf")
+library('glue')
 
 # source("Modulo/5_Bogota/src/1_mapa_manejo.R")
 
@@ -11,7 +12,9 @@ data <- read_delim(file = glue::glue("http://saludata.saludcapital.gov.co/osb/\\
                               -COVID-19.csv"), ";", skip = 4, 
                    locale = locale('es', encoding="ISO-8859-1", asciify=TRUE))
 
-write_csv(data, glue::glue("Modulo/5_Bogota/data/raw/rawdata_{today()}.csv"))
+data <- data[1:(nrow(data)-2),]
+
+write_csv(data, glue("Modulo/5_Bogota/data/raw/rawdata_{today()}.csv"))
 
 
 data1 <- data %>% 
@@ -19,11 +22,12 @@ data1 <- data %>%
   filter(!is.na(ID)) %>% 
   mutate(Fecha = dmy(Fecha),
          Sexo = factor(Sexo))
-  
-write_csv(data1, 
-          glue::glue("Modulo/5_Bogota/data/processed/prodata_{today()}.csv"))
 
+write_csv(data1, glue("Modulo/5_Bogota/data/processed/ \\
+                      prodata_{today()}.csv"))
 
+#-------------------------------------------------------------------------------#
+theme_set(theme_bw())
 
 data1 %>% 
   group_by(Fecha) %>% 
@@ -33,16 +37,51 @@ data1 %>%
   geom_bar(stat = "identity")
 
 
-data1 %>% 
+#-------------------------------------------------------------------------------#
+# Función de creación de polígonos en GGplto2
+poly_fun <- function(xmin,xmax,alpha) {
+  list(geom_rect(aes(xmin = as.Date(xmin),
+                     xmax = as.Date(xmax), 
+                     ymin = 0, ymax=200), col = NA, 
+                 fill = alpha("#84898C", alpha)))
+}
+
+textdf = tribble(
+  ~x, ~y, ~label, ~col,
+  "2020-03-26", 115, "Cuarentena nacional", 'black',
+  "2020-04-29", 115, "Inicio reactivación", 'red3'
+)
+
+G1 <- data1 %>% 
   group_by(Localidad, Fecha) %>% 
   summarise(n = n()) %>% 
   mutate(ntot = cumsum(n)) %>% 
-  ggplot(aes(x = Fecha, y = n)) + 
-  geom_bar(stat = "identity") + 
-  facet_wrap(~Localidad)
+  filter(!(Localidad %in% c('Sumapaz', 'Sin Dato', 'Fuera de Bogotá'))) %>% 
+  ggplot(aes(x = Fecha, y = n, group = Localidad)) + 
+  facet_wrap(~Localidad) + 
+  geom_vline(xintercept = as.Date('2020-03-20'), lty = 'dotted') +
+  geom_vline(xintercept = as.Date('2020-03-24'), lty = 'solid') +
+  geom_vline(xintercept = as.Date('2020-04-13'), lty = 'dashed') +
+  geom_vline(xintercept = as.Date('2020-04-21'), lty = 'dashed') +
+  
+  geom_vline(xintercept = as.Date('2020-04-27'), lty = 'dashed', col = 'red3') +
+  
+  geom_vline(xintercept = as.Date('2020-06-01'), lty = 'dashed') +
+  ylab('Número de reportes de casos Covid-19 diarios') +
+  # Línea
+  geom_line(colour = '#658dc6') + 
+  geom_text(data = textdf, aes(x=as.Date(x),y=y,label=label, col=col), angle=90,
+            size=2.5, inherit.aes = FALSE) + 
+  scale_fill_viridis_d() +
+  scale_color_manual(values = setNames(c('black', 'red3'), c('black', 'red3'))) +
+  theme(legend.position = 'none', 
+        axis.title.x = element_blank(), 
+        panel.grid = element_line(colour = 'gray99')) +
+  coord_cartesian(ylim = c(0,200))
 
 
-
+ggsave(glue('Modulo/5_Bogota/Figuras/Bogota_{today()}.png'), G1, 
+       width = 10, height = 6, device = 'png', dpi = 300)
 
 
 
